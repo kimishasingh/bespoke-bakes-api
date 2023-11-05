dc953dec9412c12fe341e51452d1cb59cd653aa8
kimisha.singh <kimishasingh@gmail.com>
2023-11-05 16:46:13 +0200

    Fix sql query

--- src/main/java/com/bespoke/bakes/repository/UserRoleRepository.java
+++ src/main/java/com/bespoke/bakes/repository/UserRoleRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
 
-    @Query("SELECT ur FROM user_role_tbl ur" +
+    @Query("SELECT ur FROM user_role_tbl ur " +
             "LEFT JOIN role_tbl r on r.id = ur.roleId " +
             "WHERE ur.userId = ?1 and r.name = ?2")
     List<UserRole> findByUserIdAndRoleName(Long userId, String roleName);
